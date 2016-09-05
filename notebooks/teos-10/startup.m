b0efebf35268843ade0ac64a6384edd8843cdb19
Nancy Soontiens <9750865+nsoontie@users.noreply.github.com>
2016-09-05 13:30:04 -0700

    Added comparison of our reference salinity with matlabs

--- notebooks/teos-10/startup.m
+++
@@ -1,6 +0,0 @@
-function [] = startup()
-  addpath /ocean/rich/home/matlab/gsw3
-  addpath /ocean/rich/home/matlab/gsw3/html
-  addpath /ocean/rich/home/matlab/gsw3/library
-  addpath /ocean/rich/home/matlab/gsw3/thermodynamics_from_t
-  addpath /ocean/rich/home/matlab/gsw3/pdf
