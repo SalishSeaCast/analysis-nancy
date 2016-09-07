a5bfb1fb5421f0cfcd04652b9eb65b874831a224
Nancy Soontiens <9750865+nsoontie@users.noreply.github.com>
2016-09-07 14:22:13 -0700

    Rewrote so that date range is a command line argument

--- notebooks/misc_tools/transfer_winds.sh
+++ notebooks/misc_tools/transfer_winds.sh
@@ -1,12 +1,14 @@
 # Script to transfer operational winds from /results to orcinus
 # Uses scp to transfer the files
+# Usage: bash transfer_winds.sh 2016-07-27 2016-08-21
+# transfers weather files in interval [July 27, 2016, August 21, 2016)
 
 DEST=/home/sallen/MEOPAR/GEM2.5/ops/NEMO-atmos/
 SOURCE=/results/forcing/atmospheric/GEM2.5/operational/
 
 #start and end dates of the transfer period
-start=2015-07-01
-end=2015-08-18
+start=$1
+end=$2
 
 d=$(date -I -d "$start") || exit -1
 end_date=$(date -I -d "$end") || exit -1
