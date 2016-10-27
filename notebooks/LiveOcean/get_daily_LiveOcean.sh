fd82fdf82c5c353188f43110224acb0b47c909b0
Nancy Soontiens <9750865+nsoontie@users.noreply.github.com>
2016-10-27 11:11:56 -0700

    moved record dimension into LiveOcean_BCs.py

--- notebooks/LiveOcean/get_daily_LiveOcean.sh
+++ notebooks/LiveOcean/get_daily_LiveOcean.sh
@@ -2,11 +2,7 @@
 # For use in cron
 
 DATE=`date +%Y-%m-%d`
-DEST=/ocean/nsoontie/MEOPAR/LiveOcean/subdomain_files
+DEST=/results/forcing/LiveOcean/downloaded/
 bash download_day.sh $DATE $DEST
 
 /home/nsoontie/anaconda3/envs/analysis/bin/python LiveOcean_BCs.py $DATE
-SAVEDIR=/ocean/nsoontie/MEOPAR/LiveOcean/boundary_files/$DATE
-for file in $SAVEDIR/*.nc; do
-    ncks -O --mk_rec_dmn=time_counter $file $file
-done
